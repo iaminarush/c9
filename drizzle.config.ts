68951e1b45a3e50f3675de08e177f59354103ac1
Patrick <patrickcheng1995@gmail.com>
2023-08-24 17:34:49 +0800

    dotenv

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
 import { Config } from "drizzle-kit";
-import { env } from "./src/env.mjs";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default {
   schema: "./src/server/db/schema/*.ts",
   driver: "pg",
   dbCredentials: {
-    // connectionString: process.env.DATABASE_URL!,
-    connectionString: "",
+    connectionString: process.env.DATABASE_URL!,
   },
 } satisfies Config;
