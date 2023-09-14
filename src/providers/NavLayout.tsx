a29a2e4c56f6c0da23b0d1369c8ee00081f6b457
Patrick <patrickcheng1995@gmail.com>
2023-09-14 16:55:13 +0800

    readding files

---
+++ src/providers/NavLayout.tsx
@@ -0,0 +1,61 @@
+"use client";
+import {
+  AppShell,
+  AppShellHeader,
+  AppShellNavbar,
+  Box,
+  Burger,
+  Container,
+  Group,
+  UnstyledButton,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { ReactNode } from "react";
+import classes from "./NavLayout.module.css";
+
+export default function NavLayout({ children }: { children: ReactNode }) {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{ height: 60 }}
+      footer={{ height: 0 }}
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { desktop: true, mobile: !opened },
+      }}
+      // styles={{ main: { minHeight: "100vh", paddingBottom: 16 } }}
+      padding="md"
+    >
+      <AppShellHeader>
+        <Group h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Group justify="flex-end" style={{ flex: 1 }}>
+            <Group ml="xl" gap={0} visibleFrom="sm">
+              <UnstyledButton className={classes.control}>Home</UnstyledButton>
+              <UnstyledButton className={classes.control}>Blog</UnstyledButton>
+              <UnstyledButton className={classes.control}>
+                Contacts
+              </UnstyledButton>
+              <UnstyledButton className={classes.control}>
+                Support
+              </UnstyledButton>
+            </Group>
+          </Group>
+        </Group>
+      </AppShellHeader>
+
+      <AppShellNavbar py="md" px={4}>
+        <UnstyledButton className={classes.control}>Home</UnstyledButton>
+        <UnstyledButton className={classes.control}>Blog</UnstyledButton>
+        <UnstyledButton className={classes.control}>Contacts</UnstyledButton>
+        <UnstyledButton className={classes.control}>Support</UnstyledButton>
+      </AppShellNavbar>
+
+      <AppShell.Main>
+        <Container>{children}</Container>
+      </AppShell.Main>
+    </AppShell>
+  );
+}
---
+++ src/providers/theme.ts
@@ -0,0 +1,21 @@
+// "use client";
+import { createTheme } from "@mantine/core";
+
+const fontFamily = [
+  "Inter",
+  "Open Sans",
+  "-apple-system",
+  "BlinkMacSystemFont",
+  "Segoe UI",
+  "Roboto",
+  "Helvetica Neue",
+  "Arial",
+  "sans-serif",
+  "Apple Color Emoji",
+  "Segoe UI Emoji",
+  "Segoe UI Symbol",
+].join(",");
+
+export const mantineTheme = createTheme({
+  fontFamily,
+});
