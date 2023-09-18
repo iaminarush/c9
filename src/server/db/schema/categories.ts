fa0e1acab3761acbc24e1b9bc40a7bbc18e69c8c
Patrick <patrickcheng1995@gmail.com>
2023-09-18 13:19:42 +0800

    add zodschema

--- src/server/db/schema/categories.ts
+++ src/server/db/schema/categories.ts
@@ -1,12 +1,22 @@
 import { relations } from "drizzle-orm";
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import {
+  AnyPgColumn,
+  integer,
+  pgTable,
+  serial,
+  text,
+} from "drizzle-orm/pg-core";
 import { items } from "./items";
+import { createSelectSchema } from "drizzle-zod";
 
 export const categories = pgTable("categories", {
   id: serial("id").primaryKey(),
   name: text("name").notNull().unique(),
+  parentId: integer("parent_id").references((): AnyPgColumn => categories.id),
 });
 
 export const categoriesRelations = relations(categories, ({ many }) => ({
   items: many(items),
 }));
+
+export const categorySchema = createSelectSchema(categories);
