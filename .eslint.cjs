4a67e217840ef0d24fb437de45caa419809497f5
Patrick <patrickcheng1995@gmail.com>
2023-09-18 11:49:50 +0800

    add typescript eslint

---
+++ .eslint.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
