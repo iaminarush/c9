b48c9bb48b5b148fe9996406ccfc4d11a688673a
Patrick <patrickcheng1995@gmail.com>
2023-09-18 16:14:08 +0800

    add all schemas

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -3,8 +3,14 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "@/env.mjs";
 import * as categories from "./schema/categories";
 import * as items from "./schema/items";
+import * as accounts from "./schema/accounts";
+import * as prices from "./schema/prices";
+import * as stores from "./schema/stores";
 
 neonConfig.fetchConnectionCache = true;
 
+const schema = { ...accounts, ...categories, ...items, ...prices, ...stores };
+
 const sql = neon(env.DATABASE_URL);
-export const db = drizzle(sql, { schema: { ...categories, ...items } });
+export const db = drizzle(sql, { schema });
+// export const db = drizzle(sql);
