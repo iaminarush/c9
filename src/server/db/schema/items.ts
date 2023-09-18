d5eb0dc1061b44fc8e58a74a6188eb7bf6ecb855
Patrick <patrickcheng1995@gmail.com>
2023-09-18 19:09:03 +0800

    fix incorrect id

--- src/server/db/schema/items.ts
+++ src/server/db/schema/items.ts
@@ -1,20 +1,26 @@
 import { relations } from "drizzle-orm";
-import { pgTable, serial, text } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
 import { categories } from "./categories";
 import { prices } from "./prices";
 import { createSelectSchema } from "drizzle-zod";
+import { z } from "zod";
 
 export const items = pgTable("items", {
   id: serial("id").primaryKey(),
   name: text("name").notNull().unique(),
+  category: integer("category_id")
+    .references(() => categories.id)
+    .notNull(),
 });
 
 export const itemsRelations = relations(items, ({ one, many }) => ({
   category: one(categories, {
-    fields: [items.id],
+    fields: [items.category],
     references: [categories.id],
   }),
   prices: many(prices),
 }));
 
 export const itemSchema = createSelectSchema(items);
+
+export const createItemSchema = itemSchema.omit({ id: true });
