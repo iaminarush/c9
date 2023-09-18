6226c73dded3145c7c0878185c567c59dc8dbc80
Patrick <patrickcheng1995@gmail.com>
2023-09-18 16:13:08 +0800

    update schema path

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export default {
-  schema: "./src/server/db/schema/*.ts",
+  schema: "./src/server/db/schema/*",
   driver: "pg",
   out: "./drizzle",
   dbCredentials: {
