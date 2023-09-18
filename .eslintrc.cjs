4ef31ad72d17984666ff0d2662e0271019ba32d0
Patrick <patrickcheng1995@gmail.com>
2023-09-18 16:12:57 +0800

    setup typescript eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,18 @@
 module.exports = {
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
     "next/core-web-vitals",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
   plugins: ["@typescript-eslint"],
   root: true,
+  rules: {
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
 };
