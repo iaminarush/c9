7b04dbd78dce9ea0b7bb6d4faa0d97fe605b3f61
Patrick <patrickcheng1995@gmail.com>
2023-09-21 15:53:35 +0800

    add packages

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { neon, neonConfig } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "@/env.mjs";
