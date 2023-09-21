4ba60f36103462ca5d52c5d50d002a85d2af10cd
Patrick <patrickcheng1995@gmail.com>
2023-09-21 15:55:03 +0800

    init

---
+++ src/providers/ToastProvider.tsx
@@ -0,0 +1,10 @@
+import { Toaster } from "react-hot-toast";
+
+export default function ToastProvider() {
+  // const colorScheme = useComputedColorScheme();
+  return (
+    <>
+      <Toaster toastOptions={{}} />
+    </>
+  );
+}
