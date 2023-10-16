2495a8d8f50bd948ecf09ca33a51b6a483625c87
Patrick <patrickcheng1995@gmail.com>
2023-10-16 11:34:08 +0800

    fix for blurry toast

--- src/providers/ToastProvider.tsx
+++ src/providers/ToastProvider.tsx
@@ -2,9 +2,5 @@ import { Toaster } from "react-hot-toast";
 
 export default function ToastProvider() {
   // const colorScheme = useComputedColorScheme();
-  return (
-    <>
-      <Toaster toastOptions={{}} />
-    </>
-  );
+  return <Toaster toastOptions={{ style: { lineHeight: "26px" } }} />;
 }
