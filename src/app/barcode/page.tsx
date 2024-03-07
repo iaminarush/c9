5e3978fd4def14a1c13f61828d627b5aeef5593f
Patrick <patrickcheng1995@gmail.com>
2024-03-07 17:44:06 +0800

    clean

--- src/app/barcode/page.tsx
+++ src/app/barcode/page.tsx
@@ -2,23 +2,39 @@
 
 import { useMediaDevices } from "react-media-devices";
 import { useZxing } from "react-zxing";
+import Webcam from "react-webcam";
+import { Stack } from "@mantine/core";
 
 export default function Barcode() {
   const { devices } = useMediaDevices({
     constraints: { video: true, audio: false },
   });
 
-  console.log(devices);
-
   const deviceId = devices?.[3]?.deviceId;
 
-  const { ref } = useZxing({ paused: !deviceId, deviceId });
+  const { ref } = useZxing({
+    paused: !deviceId,
+    deviceId,
+    onDecodeResult: (result) => console.log(result),
+  });
 
   // return <video ref={ref} height={300} width={300} />;
 
   return (
     <>
-      <video ref={ref} height={300} width={300} />
+      <Stack>
+        <video ref={ref} height={300} width={300} />
+      </Stack>
     </>
   );
 }
+
+// export default function Barcode() {
+//   const { ref } = useZxing();
+
+//   return (
+//     <>
+//       <Webcam videoConstraints={{ facingMode: "environment" }} ref={ref} />
+//     </>
+//   );
+// }
