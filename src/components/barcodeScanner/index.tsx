6776e3c0189e5e49f8cdba4fb37f9f6e79b089ed
Patrick <patrickcheng1995@gmail.com>
2024-03-08 18:03:21 +0800

    clean

--- src/components/barcodeScanner/index.tsx
+++ src/components/barcodeScanner/index.tsx
@@ -1,4 +1,4 @@
-import { Modal, Select, Stack, Text } from "@mantine/core";
+import { Loader, Modal, Select, Stack } from "@mantine/core";
 import { useState } from "react";
 import { useMediaDevices } from "react-media-devices";
 import { useZxing } from "react-zxing";
@@ -16,12 +16,13 @@ export const BarcodeScanner = ({
     constraints: { video: true, audio: false },
   });
   const [selectedDevice, setSelectedDevice] = useState<string | null>(null);
-  const [barcode, setBarcode] = useState("");
 
   const videoSources =
     devices
       ?.filter((d) => d.kind === "videoinput")
       .map((v) => ({ label: v.label, value: v.deviceId })) || [];
+      
+  const noCamera = !loading && videoSources.length === 0
 
   const { ref } = useZxing({
     paused: !selectedDevice,
@@ -34,13 +35,16 @@ export const BarcodeScanner = ({
   });
 
   return (
-    <Modal opened={opened} onClose={onClose}>
+    <Modal opened={opened} onClose={onClose} title='Scan a barcode!'>
       <Stack>
         <Select
           data={videoSources}
           label="Device"
           value={selectedDevice}
           onChange={setSelectedDevice}
+          leftSection={loading ? <Loader size="sm" /> : null}
+          placeholder={noCamera ? 'No cameras found' : undefined}
+          disabled={noCamera}
         />
         <video ref={ref} />
       </Stack>
