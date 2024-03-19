db2c3e7804bcc32953e997f80d8ef84b5692f6a0
Patrick <patrickcheng1995@gmail.com>
2024-03-19 17:38:27 +0800

    add exception to uploadthing api

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,7 @@ export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    /* * Match all request paths except for the ones starting with: * - api (API routes) * - _next/static (static files) * - _next/image (image optimization files) * - favicon.ico (favicon file) */
-    "/((?!_next/static|_next/image|favicon.ico).*)",
+    /* * Match all request paths except for the ones starting with: * - api/uploadthing Uploadthing's webhook * - _next/static (static files) * - _next/image (image optimization files) * - favicon.ico (favicon file) */
+    "/((?!api/uploadthing|_next/static|_next/image|favicon.ico).*)",
   ],
 };
