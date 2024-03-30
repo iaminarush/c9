1247ed1ece99dcac820f345c42c3306c10cfec4b
Patrick <patrickcheng1995@gmail.com>
2024-03-30 23:44:32 +0800

    update schema

--- src/server/db/schema/barcodes.ts
+++ src/server/db/schema/barcodes.ts
@@ -1,11 +1,12 @@
 import { relations } from "drizzle-orm";
 import { integer, pgTable, serial, varchar } from "drizzle-orm/pg-core";
 import { items } from "./items";
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 
 export const barcodes = pgTable("barcodes", {
   id: serial("id").primaryKey(),
-  barcode: varchar("barcode", { length: 13 }),
-  itemId: integer("item_id"),
+  barcode: varchar("barcode", { length: 13 }).notNull(),
+  itemId: integer("item_id").notNull(),
 });
 
 export const barcodesRelations = relations(barcodes, ({ one }) => ({
@@ -14,3 +15,7 @@ export const barcodesRelations = relations(barcodes, ({ one }) => ({
     references: [items.id],
   }),
 }));
+
+export const barcodeSchema = createSelectSchema(barcodes);
+
+export const createBarcodeSchema = createInsertSchema(barcodes);
