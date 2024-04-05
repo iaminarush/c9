6ddb8144d931325f523313e7101d205512ff933b
Patrick <patrickcheng1995@gmail.com>
2024-04-05 15:47:10 +0800

    remove deprecated connection cache

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -4,7 +4,7 @@ import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "@/env.mjs";
 import * as schema from "./schema";
 
-neonConfig.fetchConnectionCache = true;
+// neonConfig.fetchConnectionCache = true;
 
 const sql = neon(env.DATABASE_URL);
 export const db = drizzle(sql, {
