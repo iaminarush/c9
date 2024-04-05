bbd3b21ea9bc217d9da42721cbb8294ec17b91ab
Patrick <patrickcheng1995@gmail.com>
2024-04-05 15:53:15 +0800

    clean import

--- src/server/db/db.ts
+++ src/server/db/db.ts
@@ -1,10 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { neon, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
 import { env } from "@/env.mjs";
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-// neonConfig.fetchConnectionCache = true;
 
 const sql = neon(env.DATABASE_URL);
 export const db = drizzle(sql, {
