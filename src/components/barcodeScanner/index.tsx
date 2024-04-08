e8032114980bea00bc3fc611f498747093474ec6
Patrick <patrickcheng1995@gmail.com>
2024-04-08 11:37:34 +0800

    remove modal from component

--- src/components/barcodeScanner/index.tsx
+++ src/components/barcodeScanner/index.tsx
@@ -1,8 +1,8 @@
-import { Loader, Modal, Select, Stack } from "@mantine/core";
+import { Loader, Select, Stack } from "@mantine/core";
+import { BarcodeFormat, DecodeHintType } from "@zxing/library";
 import { useState } from "react";
 import { useMediaDevices } from "react-media-devices";
 import { useZxing } from "react-zxing";
-import { BarcodeFormat, DecodeHintType } from "@zxing/library";
 
 const hints = new Map();
 const formats = [BarcodeFormat.CODABAR];
@@ -10,12 +10,8 @@ const formats = [BarcodeFormat.CODABAR];
 hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
 export const BarcodeScanner = ({
-  opened,
-  onClose,
   handleScan,
 }: {
-  opened: boolean;
-  onClose: () => void;
   handleScan: (barcode: string) => void;
 }) => {
   const { devices, loading } = useMediaDevices({
@@ -30,33 +26,30 @@ export const BarcodeScanner = ({
 
   const noCamera = !loading && videoSources.length === 0;
 
-  console.log(!!selectedDevice);
-
   const { ref } = useZxing({
     paused: !selectedDevice,
     deviceId: selectedDevice || "",
     onDecodeResult: (result) => {
       handleScan(result.getText());
-      // onClose();
     },
-    // onDecodeError: (error) => {},
-    hints,
+    onDecodeError: (error) => {
+      console.log(error);
+    },
+    // hints,
   });
 
   return (
-    <Modal opened={opened} onClose={onClose} title="Scan a barcode!">
-      <Stack>
-        <Select
-          data={videoSources}
-          label="Device"
-          value={selectedDevice}
-          onChange={setSelectedDevice}
-          leftSection={loading ? <Loader size="sm" /> : null}
-          placeholder={noCamera ? "No cameras found" : undefined}
-          disabled={noCamera}
-        />
-        <video ref={ref} />
-      </Stack>
-    </Modal>
+    <Stack>
+      <Select
+        data={videoSources}
+        label="Device"
+        value={selectedDevice}
+        onChange={setSelectedDevice}
+        leftSection={loading ? <Loader size="sm" /> : null}
+        placeholder={noCamera ? "No cameras found" : undefined}
+        disabled={noCamera}
+      />
+      <video ref={ref} />
+    </Stack>
   );
 };
