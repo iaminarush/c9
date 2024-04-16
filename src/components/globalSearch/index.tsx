95b86d6c62901ff8dfbcfa128bb733906ad9cc8c
Patrick <patrickcheng1995@gmail.com>
2024-04-16 15:21:49 +0800

    clean

--- src/components/globalSearch/index.tsx
+++ src/components/globalSearch/index.tsx
@@ -45,52 +45,6 @@ export default function GlobalSearch() {
             : "Type at least 3 letters to start searching"
         }
       />
-
-      {/* <Spotlight.Root
-        query={query}
-        onQueryChange={setQuery}
-        maxHeight={350}
-        scrollable
-      >
-        <Spotlight.Search
-          placeholder="Search"
-          leftSection={
-            <IconSearch
-              style={{ width: rem(20), height: rem(20) }}
-              stroke={1.5}
-            />
-          }
-          rightSection={
-            searchResults.isFetching ? <Loader size={rem(20)} /> : null
-          }
-        />
-        <Spotlight.ActionsList>
-          {query.length > 2 ? (
-            !!searchResults.data &&
-            !searchResults.data.categories.length &&
-            !searchResults.data.items.length ? (
-              <SpotlightEmpty>
-                {"Couldn't find what you were looking for"}
-              </SpotlightEmpty>
-            ) : (
-              <>
-                <Spotlight.ActionsGroup label="Categories">
-                  {searchResults.data?.categories.map((c) => (
-                    <Spotlight.Action key={c.id} label={c.label}>
-                      <Link href={`/categories/${c.id}`} />
-                    </Spotlight.Action>
-                  ))}
-                </Spotlight.ActionsGroup>
-                <Spotlight.ActionsGroup label="Items"></Spotlight.ActionsGroup>
-              </>
-            )
-          ) : (
-            <Spotlight.Empty>
-              Type at least 3 letters to start searching
-            </Spotlight.Empty>
-          )}
-        </Spotlight.ActionsList>
-      </Spotlight.Root> */}
     </>
   );
 }
