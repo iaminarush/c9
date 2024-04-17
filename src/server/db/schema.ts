9466fdc2bd7ec28452ab0196ac75ac168b59040f
Patrick <patrickcheng1995@gmail.com>
2024-04-17 11:14:53 +0800

    remove prices from db

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -2,7 +2,6 @@ export * from "./schema/accounts";
 export * from "./schema/barcodes";
 export * from "./schema/categories";
 export * from "./schema/items";
-export * from "./schema/prices";
 export * from "./schema/records";
 export * from "./schema/stores";
 export * from "./schema/unitTypes";
