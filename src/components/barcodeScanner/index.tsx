4d877d3de03f92cf09a6dd54be23d63299dcf36d
Patrick <patrickcheng1995@gmail.com>
2024-06-20 17:37:58 +0800

    use react-barcode-scanner

--- src/components/barcodeScanner/index.tsx
+++ src/components/barcodeScanner/index.tsx
@@ -1,60 +1,80 @@
-import { Loader, Select, Stack, Text } from "@mantine/core";
-import { BarcodeFormat, DecodeHintType } from "@zxing/library";
-import { useState } from "react";
-import toast from "react-hot-toast";
-import { useMediaDevices } from "react-media-devices";
-import { useZxing } from "react-zxing";
+import dynamic from "next/dynamic";
 
-const hints = new Map();
-const formats = [BarcodeFormat.EAN_13];
+// const hints = new Map();
+// const formats = [BarcodeFormat.EAN_13];
 
-hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
+// hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
 
-export const BarcodeScanner = ({
+// export const BarcodeScanner = ({
+//   handleScan,
+// }: {
+//   handleScan: (barcode: string) => void;
+// }) => {
+//   const { devices, loading } = useMediaDevices({
+//     constraints: {
+//       video: true,
+//       audio: false,
+//     },
+//   });
+//   const [selectedDevice, setSelectedDevice] = useState<string | null>(null);
+//   const [error, setError] = useState("");
+
+//   const videoSources =
+//     devices
+//       ?.filter((d) => d.kind === "videoinput")
+//       .map((v) => ({ label: v.label, value: v.deviceId })) || [];
+
+//   const noCamera = !loading && videoSources.length === 0;
+
+//   const { ref } = useZxing({
+//     paused: !selectedDevice,
+//     deviceId: selectedDevice || "",
+//     onDecodeResult: (result) => {
+//       handleScan(result.getText());
+//     },
+//     onDecodeError: (error) => {
+//       // console.log(error);
+//       // toast.error(error.message);
+//       setError(error.message);
+//     },
+//     hints,
+//   });
+
+//   return (
+//     <Stack>
+//       <Select
+//         data={videoSources}
+//         label="Device"
+//         value={selectedDevice}
+//         onChange={setSelectedDevice}
+//         leftSection={loading ? <Loader size="sm" /> : null}
+//         placeholder={noCamera ? "No cameras found" : undefined}
+//         disabled={noCamera}
+//       />
+//       {!!error && <Text c="red">Error: {error}</Text>}
+//       <video ref={ref} />
+//     </Stack>
+//   );
+// };
+
+const BarcodeScannerPolyfilled = dynamic(
+  () => {
+    //@ts-expect-error Polyfill does not have types module
+    import("react-barcode-scanner/polyfill");
+    return import("react-barcode-scanner").then((mod) => mod.BarcodeScanner);
+  },
+  { ssr: false },
+);
+
+export default function BarcodeScanner({
   handleScan,
 }: {
-  handleScan: (barcode: string) => void;
-}) => {
-  const { devices, loading } = useMediaDevices({
-    constraints: { video: true, audio: false },
-  });
-  const [selectedDevice, setSelectedDevice] = useState<string | null>(null);
-  const [error, setError] = useState("");
-
-  const videoSources =
-    devices
-      ?.filter((d) => d.kind === "videoinput")
-      .map((v) => ({ label: v.label, value: v.deviceId })) || [];
-
-  const noCamera = !loading && videoSources.length === 0;
-
-  const { ref } = useZxing({
-    paused: !selectedDevice,
-    deviceId: selectedDevice || "",
-    onDecodeResult: (result) => {
-      handleScan(result.getText());
-    },
-    onDecodeError: (error) => {
-      // console.log(error);
-      // toast.error(error.message);
-      setError(error.message);
-    },
-    hints,
-  });
-
+  handleScan: (code: string) => void;
+}) {
   return (
-    <Stack>
-      <Select
-        data={videoSources}
-        label="Device"
-        value={selectedDevice}
-        onChange={setSelectedDevice}
-        leftSection={loading ? <Loader size="sm" /> : null}
-        placeholder={noCamera ? "No cameras found" : undefined}
-        disabled={noCamera}
-      />
-      {!!error && <Text c="red">Error: {error}</Text>}
-      <video ref={ref} />
-    </Stack>
+    <BarcodeScannerPolyfilled
+      onCapture={({ rawValue }) => handleScan(rawValue)}
+      options={{ formats: ["ean_13"] }}
+    />
   );
-};
+}
