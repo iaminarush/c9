0cfde054d07a22ef316965b476602623cd342122
Patrick <patrickcheng1995@gmail.com>
2024-06-21 15:07:09 +0800

    remove old code

--- src/components/barcodeScanner/index.tsx
+++ src/components/barcodeScanner/index.tsx
@@ -1,67 +1,11 @@
 import dynamic from "next/dynamic";
 
-// const hints = new Map();
-// const formats = [BarcodeFormat.EAN_13];
-
-// hints.set(DecodeHintType.POSSIBLE_FORMATS, formats);
-
-// export const BarcodeScanner = ({
-//   handleScan,
-// }: {
-//   handleScan: (barcode: string) => void;
-// }) => {
-//   const { devices, loading } = useMediaDevices({
-//     constraints: {
-//       video: true,
-//       audio: false,
-//     },
-//   });
-//   const [selectedDevice, setSelectedDevice] = useState<string | null>(null);
-//   const [error, setError] = useState("");
-
-//   const videoSources =
-//     devices
-//       ?.filter((d) => d.kind === "videoinput")
-//       .map((v) => ({ label: v.label, value: v.deviceId })) || [];
-
-//   const noCamera = !loading && videoSources.length === 0;
-
-//   const { ref } = useZxing({
-//     paused: !selectedDevice,
-//     deviceId: selectedDevice || "",
-//     onDecodeResult: (result) => {
-//       handleScan(result.getText());
-//     },
-//     onDecodeError: (error) => {
-//       // console.log(error);
-//       // toast.error(error.message);
-//       setError(error.message);
-//     },
-//     hints,
-//   });
-
-//   return (
-//     <Stack>
-//       <Select
-//         data={videoSources}
-//         label="Device"
-//         value={selectedDevice}
-//         onChange={setSelectedDevice}
-//         leftSection={loading ? <Loader size="sm" /> : null}
-//         placeholder={noCamera ? "No cameras found" : undefined}
-//         disabled={noCamera}
-//       />
-//       {!!error && <Text c="red">Error: {error}</Text>}
-//       <video ref={ref} />
-//     </Stack>
-//   );
-// };
-
 const BarcodeScannerPolyfilled = dynamic(
   () => {
     //@ts-expect-error Polyfill does not have types module
     import("react-barcode-scanner/polyfill");
-    return import("react-barcode-scanner").then((mod) => mod.BarcodeScanner);
+    // return import("react-barcode-scanner").then((mod) => mod.BarcodeScanner);
+    return import("./custom-barcode-scanner").then((mod) => mod.default);
   },
   { ssr: false },
 );
@@ -74,7 +18,7 @@ export default function BarcodeScanner({
   return (
     <BarcodeScannerPolyfilled
       onCapture={({ rawValue }) => handleScan(rawValue)}
-      options={{ formats: ["ean_13"] }}
+      options={{ formats: ["ean_13"], delay: 1000 }}
     />
   );
 }
