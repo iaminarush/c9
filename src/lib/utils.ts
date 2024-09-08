b352d40aa87c6cb6d3c04c24358945cba2094210
Patrick <patrickcheng1995@gmail.com>
2024-09-08 18:21:43 +0800

    add roundto function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,3 +6,19 @@ export const isNumber = (value: string | number) => {
     return !isNaN(value);
   }
 };
+
+/**
+ * Rounds a number up to the specified number of decimal places.
+ *
+ * @param value - The number to be rounded.
+ * @param precision - The number of decimal places to round to.
+ * @returns The rounded number.
+ */
+export const roundTo = (value: number, precision = 2) => {
+  if (precision < 0) {
+    throw new Error("Precision must be a non-negative integer.");
+  }
+
+  const factor = Math.pow(10, precision);
+  return Math.ceil(value * factor) / factor;
+};
