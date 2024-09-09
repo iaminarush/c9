7aa5a880d8c34a6ffe1a24869b36f9fd601be70c
Patrick <patrickcheng1995@gmail.com>
2024-09-10 00:56:50 +0800

    fix mutations

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -1,7 +1,32 @@
 "use client";
-import { Skeleton, Stack, Text } from "@mantine/core";
-import { InventoryCard } from "../items/[id]/inventory";
-import { useAllInventory } from "./query";
+import DateFormField from "@/components/hook-form/DateFormField";
+import NumberFormField from "@/components/hook-form/NumberFormField";
+import {
+  inventorySchema,
+  updateInventorySchema,
+} from "@/server/db/schema/inventory";
+import {
+  ActionIcon,
+  Badge,
+  Button,
+  Card,
+  Group,
+  Modal,
+  Skeleton,
+  Stack,
+  Text,
+} from "@mantine/core";
+import { useDisclosure, useMediaQuery } from "@mantine/hooks";
+import { IconCheck, IconEdit, IconTrash, IconX } from "@tabler/icons-react";
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { useSession } from "next-auth/react";
+import { ReactNode } from "react";
+import { SubmitHandler, useForm, useWatch } from "react-hook-form";
+import { z } from "zod";
+import { useAllInventory, useDeleteInventory, useEditInventory } from "./query";
+
+dayjs.extend(relativeTime);
 
 export default function Inventory() {
   const inventory = useAllInventory();
@@ -32,3 +57,165 @@ export default function Inventory() {
     </Stack>
   );
 }
+
+type Inventory = z.infer<typeof inventorySchema>;
+
+const InventoryCard = ({
+  item,
+  ...inventory
+}: Inventory & { item?: ReactNode }) => {
+  const [edit, { open, close }] = useDisclosure(false);
+  const matches = useMediaQuery("(min-width: 36em)");
+  const expiryDate = dayjs(inventory.expiryDate);
+  const today = dayjs();
+  const { data } = useSession();
+
+  const timeLeft = expiryDate.isSame(today, "day")
+    ? "Expiring today!"
+    : expiryDate.isBefore(today, "day")
+    ? "Expired :("
+    : `Expiring ${expiryDate.fromNow()}`;
+
+  const difference = expiryDate.diff(today, "day");
+
+  return (
+    <Card p="xs">
+      {!edit ? (
+        <Stack>
+          {item}
+          <Group justify="space-between" wrap="nowrap" gap={"xs"}>
+            <Group align="flex-start">
+              <Stack gap="xs">
+                <Text fw={700}>{matches ? "Quantity" : "Qty"}</Text>
+                <Text>{inventory.quantity}</Text>
+              </Stack>
+
+              <Stack gap="xs" justify="flex-start">
+                <Text fw={700}>Expiry</Text>
+                <Text>{dayjs(inventory.expiryDate).format("YYYY-MM-DD")}</Text>
+                {difference <= 3 && difference >= 0 ? (
+                  <Badge color="red">{timeLeft}</Badge>
+                ) : (
+                  <Text>{timeLeft}</Text>
+                )}
+              </Stack>
+            </Group>
+
+            <Group>
+              <Stack>
+                <ActionIcon onClick={open} disabled={!data?.user.admin}>
+                  <IconEdit />
+                </ActionIcon>
+
+                <DeleteComponent id={inventory.id} />
+              </Stack>
+            </Group>
+          </Group>
+        </Stack>
+      ) : (
+        <EditForm {...inventory} close={close} />
+      )}
+    </Card>
+  );
+};
+
+const DeleteComponent = ({ id }: { id: number }) => {
+  const [opened, { open, close }] = useDisclosure(false);
+  const { data } = useSession();
+
+  const { mutate, isLoading } = useDeleteInventory();
+
+  const handleClick = () => {
+    mutate(
+      { params: { id: `${id}` }, body: null },
+      { onSuccess: () => close() },
+    );
+  };
+
+  return (
+    <>
+      <ActionIcon
+        color="red"
+        variant="filled"
+        onClick={open}
+        disabled={!data?.user.admin}
+      >
+        <IconTrash />
+      </ActionIcon>
+
+      <Modal opened={opened} onClose={close} title="Delete this item?" centered>
+        <Stack>
+          <Button
+            color="red"
+            variant="filled"
+            onClick={handleClick}
+            loading={isLoading}
+          >
+            Delete
+          </Button>
+        </Stack>
+      </Modal>
+    </>
+  );
+};
+
+const editFormSchema = updateInventorySchema.merge(
+  z.object({ expiryDate: z.date(), quantity: z.number() }),
+);
+
+type EditFormSchema = z.infer<typeof editFormSchema>;
+
+const EditForm = ({
+  close,
+  ...inventory
+}: Inventory & { close: () => void }) => {
+  const { control, handleSubmit } = useForm<EditFormSchema>({
+    defaultValues: {
+      ...inventory,
+      quantity: Number(inventory.quantity),
+      expiryDate: dayjs(inventory.expiryDate).toDate(),
+    },
+  });
+  const { mutate, isLoading } = useEditInventory();
+
+  const onSubmit: SubmitHandler<EditFormSchema> = (data) => {
+    const submitData = {
+      quantity: `${data.quantity}`,
+      expiryDate: data.expiryDate.toISOString(),
+    };
+
+    mutate(
+      { body: submitData, params: { id: `${inventory.id}` } },
+      { onSuccess: () => close() },
+    );
+  };
+
+  const watch = useWatch({ control, name: "quantity" });
+  console.log(watch);
+
+  return (
+    <Group justify="space-between">
+      <Group>
+        <NumberFormField control={control} name="quantity" label="Quantity" />
+        <DateFormField control={control} name="expiryDate" label="Expiry" />
+      </Group>
+      <Stack>
+        <ActionIcon
+          variant="filled"
+          color="red"
+          onClick={close}
+          loading={isLoading}
+        >
+          <IconX />
+        </ActionIcon>
+        <ActionIcon
+          variant="filled"
+          onClick={handleSubmit(onSubmit)}
+          loading={isLoading}
+        >
+          <IconCheck />
+        </ActionIcon>
+      </Stack>
+    </Group>
+  );
+};
--- src/app/inventory/query.ts
+++ src/app/inventory/query.ts
@@ -1,4 +1,8 @@
 import { client } from "@/contracts/contract";
+import { inventoryContract } from "@/contracts/contract-inventory";
+import { useQueryClient } from "@tanstack/react-query";
+import { ServerInferResponses } from "@ts-rest/core";
+import { produce } from "immer";
 
 const keys = {
   all: ["inventory"],
@@ -6,3 +10,57 @@ const keys = {
 
 export const useAllInventory = () =>
   client.inventory.getAllInventory.useQuery(keys.all);
+
+type InventoriesResponse = ServerInferResponses<
+  typeof inventoryContract.getInventories,
+  200
+>;
+
+export const useDeleteInventory = () => {
+  const queryClient = useQueryClient();
+
+  return client.inventory.deleteInventory.useMutation({
+    onSuccess: ({ body }) => {
+      queryClient.setQueryData<InventoriesResponse>(keys.all, (oldData) => {
+        console.log(oldData);
+        if (!oldData) return undefined;
+
+        const index = oldData.body.findIndex((i) => i.id === body.id);
+
+        if (index !== -1) {
+          const newData = produce(oldData, (draft) => {
+            draft.body.splice(index, 1);
+          });
+
+          return newData;
+        }
+
+        return oldData;
+      });
+    },
+  });
+};
+
+export const useEditInventory = () => {
+  const queryClient = useQueryClient();
+
+  return client.inventory.editInventory.useMutation({
+    onSuccess: ({ body }) => {
+      queryClient.setQueryData<InventoriesResponse>(keys.all, (oldData) => {
+        if (!oldData) return undefined;
+
+        const index = oldData.body.findIndex((i) => i.id === body.id);
+
+        if (index !== -1) {
+          const newData = produce(oldData, (draft) => {
+            draft.body[index] = body;
+          });
+
+          return newData;
+        }
+
+        return oldData;
+      });
+    },
+  });
+};
