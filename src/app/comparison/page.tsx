79dd246eca37ff6cfcb7b6e8ba7cb78f0ccef628
Patrick <patrickcheng1995@gmail.com>
2024-10-27 22:43:45 +0800

    change todo formatting for nvim

--- src/app/comparison/page.tsx
+++ src/app/comparison/page.tsx
@@ -4,10 +4,7 @@ import NumberFormField from "@/components/hook-form/NumberFormField";
 import SelectFormField from "@/components/hook-form/SelectFormField";
 import SwitchFormField from "@/components/hook-form/SwitchFormField";
 import { UnitTypes } from "@/contracts/contract-unitType";
-import {
-  useUnitFamiliesData,
-  useUnitTypesData
-} from "@/lib/commonQueries";
+import { useUnitFamiliesData, useUnitTypesData } from "@/lib/commonQueries";
 import { isNumber } from "@/lib/utils";
 import { useAutoAnimate } from "@formkit/auto-animate/react";
 import {
@@ -51,7 +48,8 @@ export default function Comparison() {
 
   const unitTypes = useUnitTypesData({});
   const unitFamilies = useUnitFamiliesData({});
-  //TODO
+  //TODO:
+
   // const kek = useUnitFamilies({
   //   queryOptions: {
   //     select: ({ body }) =>
